0e236507f25901d58ee1fb4ca8bb9640e9d2c565
Sujay Patil <sujupa@gmail.com>
2021-12-14 10:45:38 +0530

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Sujay Patil on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
